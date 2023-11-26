0fab2b6e8f8af1a43f7cf5c8298e19afbd8c9ec4
braydenperry <braydenperry1@mail.weber.edu>
2023-11-25 19:11:04 -0700

    Reduce code duplication

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,5 +1,4 @@
 import WeatherCard from '../WeatherCard/WeatherCard';
-
 import './Weather.css';
 
 const Weather = ({ place, forecastData, currentData }) => {
@@ -9,27 +8,16 @@ const Weather = ({ place, forecastData, currentData }) => {
         {place.name}, {place.region}
       </div>
       <div className="card-row">
-        <WeatherCard
-          date={new Date(forecastData.forecastday[0].date)}
-          condition={currentData.condition}
-          mainTemp={currentData.temp_c}
-          minTemp={forecastData.forecastday[0].day.mintemp_c}
-          maxTemp={forecastData.forecastday[0].day.maxtemp_c}
-        />
-        <WeatherCard
-          date={new Date(forecastData.forecastday[1].date)}
-          condition={forecastData.forecastday[1].day.condition}
-          mainTemp={forecastData.forecastday[1].day.avgtemp_c}
-          minTemp={forecastData.forecastday[1].day.mintemp_c}
-          maxTemp={forecastData.forecastday[1].day.maxtemp_c}
-        />
-        <WeatherCard
-          date={new Date(forecastData.forecastday[2].date)}
-          condition={forecastData.forecastday[2].day.condition}
-          mainTemp={forecastData.forecastday[2].day.avgtemp_c}
-          minTemp={forecastData.forecastday[2].day.mintemp_c}
-          maxTemp={forecastData.forecastday[2].day.maxtemp_c}
-        />
+        {forecastData.forecastday.map((day, index) => (
+          <WeatherCard
+            key={index}
+            date={new Date(day.date)}
+            condition={index === 0 ? currentData.condition : day.day.condition}
+            mainTemp={index === 0 ? currentData.temp_c : day.day.avgtemp_c}
+            minTemp={day.day.mintemp_c}
+            maxTemp={day.day.maxtemp_c}
+          />
+        ))}
       </div>
     </div>
   );
